ed07eeff32a2c258a6921a2a99b70629f222fc45
raphaeldamasceno07 <raphael_damasceno07@icloud.com>
2025-11-30 20:52:37 -0300

    add `compose` config file with database service

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  console.log(response.headers);
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ status: "são os melhores" });
 }
 
