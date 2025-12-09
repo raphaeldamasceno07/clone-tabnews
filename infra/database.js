2ec0976cc902d61253febac43361c6c72b4f97e8
raphaeldamasceno07 <raphael_damasceno07@icloud.com>
2025-12-09 18:47:52 -0300

    makes `database.js` `client.connect()` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,4 @@
-import { Pool, Client } from "pg";
+import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
@@ -9,13 +9,21 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
-  client.connect();
+  console.log("Credenciais do PostgreSQL:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
