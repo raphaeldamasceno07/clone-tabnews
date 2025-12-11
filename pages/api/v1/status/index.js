88b4e98d86be8534ede0c25392ccf0f0112bd2c1
raphaeldamasceno07 <raphael_damasceno07@icloud.com>
2025-12-11 18:04:46 -0300

    handles database ssl connection

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,14 +2,20 @@ import database from "infra/database.js";
 
 async function status(request, response) {
   const updateAt = new Date().toISOString();
+
   const databseVersionResult = await database.query("SHOW server_version;");
+
   const databaseVersionValue = databseVersionResult.rows[0].server_version;
+
   const databaseMaxConnectionsResult = await database.query(
     "SHOW max_connections;",
   );
+
   const databaseName = process.env.POSTGRES_DB;
+
   const databaseMaxConnectionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
+
   const openedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
